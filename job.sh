a8f92b9ee3572a32854fb364f73cbd8790938cf5
Bryant <tangyu990628@gmail.com>
2023-10-13 18:43:15 +0200

    trial-1

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SBATCH -n 16                              # Number of cores
-SBATCH --time=80:00:00                      # hours:minutes:seconds
-SBATCH --mem-per-cpu=2000
+SBATCH -n 20                             # Number of cores
+SBATCH --time=3-5                      # hours:minutes:seconds
+SBATCH --mem-per-cpu=40G
 SBATCH --tmp=4000                        # per node!!
 SBATCH --job-name=bkt_cr4
 SBATCH --output=./bkt_cr4.out
