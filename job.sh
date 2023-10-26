7f96b77c8d43778b8530a5cbee908c67fd3753b0
Bryant <tangyu990628@gmail.com>
2023-10-26 20:28:51 +0200

    job.sh

--- job.sh
+++ job.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-SBATCH -n 20                             # Number of cores
-SBATCH --time=3-5                      # hours:minutes:seconds
-SBATCH --mem-per-cpu=40G
-SBATCH --tmp=4000                        # per node!!
-SBATCH --job-name=bkt_cr4
-SBATCH --output=./bkt_cr4.out
-SBATCH --error=./bkt_cr4.err
+# SBATCH -n 20                             # Number of cores
+# SBATCH --time=3-5                      # hours:minutes:seconds
+# SBATCH --mem-per-cpu=40G
+# SBATCH --tmp=4000                        # per node!!
+# SBATCH --job-name=bkt_cr4
+# SBATCH --output=./bkt_cr4.out
+# SBATCH --error=./bkt_cr4.err
 
+SBATCH --mail-type=ALL                           # mail configuration: NONE, BEGIN, END, FAIL, REQUEUE, ALL
+SBATCH --output=/absolute/path/to/log/%j.out     # where to store the output (%j is the JOBID), subdirectory "log" must exist
+SBATCH --error=/absolute/path/to/log/log/%j.err  # where to store error messages
 # run experiment
 python ./BasicForgets.py
