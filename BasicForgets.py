b1c8fe269b3d8d116a46c2ebcae0c8fd9f3ffc98
Bryant <tangyu990628@gmail.com>
2023-10-29 16:33:47 +0100

    static_dataset

--- BasicForgets.py
+++ BasicForgets.py
@@ -6,7 +6,10 @@ np.seterr(divide='ignore', invalid='ignore')
 
 if __name__ == '__main__':
     model = Model(seed = 0, num_fits = 10)
-    model.fit(data_path = "data/course4_bkt_train.csv")
+    model.fit(data_path = "data/comb_df_1_train.csv")
+    print(model.params())
+    preds_df = model.predict(data_path = 'comb_df_1_train.csv') # prediction on training set
+    preds_df.to_csv('pred_df1.csv', index=False)
     print("Standard BKT:", model.evaluate(data_path = "data/course4_bkt_test.csv", metric="auc"))
     # model2 = Model(seed = 0, num_fits = 20)
     # model2.fit(data_path = "data/builder_train_preprocessed.csv", forgets=True)
