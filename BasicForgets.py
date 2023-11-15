2ff1cb1e92a695abbebcbd9fea7c6ac04bc14dda
Yu Tang <tangyu990628@gmail>
2023-11-15 18:12:12 +0100

    df1_new

--- BasicForgets.py
+++ BasicForgets.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     # project="BKT",
     # name="trial-df1",)
     # Open a file for writing
-    logging.basicConfig(filename='output_df1.log', level=logging.INFO)
+    logging.basicConfig(filename='bkt_sem1.log', level=logging.INFO)
     logger = logging.getLogger()
 
     # Now, log messages to the file
@@ -22,24 +22,31 @@ if __name__ == '__main__':
     # original_stdout = sys.stdout
     # # Redirect sys.stdout to the file
     # sys.stdout = steam_file
-    model = Model(seed = 0, num_fits = 5)
-    model.fit(data_path = "data/comb_df_1_train.csv")
+    model = Model(seed = 0, num_fits = 10)
+    model.fit(data_path = "data/df1_bkt_train.csv")
     print(model.params())
     # preds_df = model.predict(data_path = 'data/course4_bkt_train.csv') # prediction on training set
     # preds_df.to_csv('pred_statics.csv', index=False)
-    train_result="Standard BKT (train_auc):"+str(model.evaluate(data_path = "data/comb_df_1_train.csv", metric="auc"))
-    valid_result="Standard BKT (valid_auc):"+str(model.evaluate(data_path = "data/comb_df_1_test.csv", metric="auc"))
+    train_result="Standard BKT (train_auc):"+str(model.evaluate(data_path = "data/df1_bkt_train.csv", metric="auc"))
+    valid_result="Standard BKT (valid_auc):"+str(model.evaluate(data_path = "data/df1_bkt_test.csv", metric="auc"))
     print(train_result)
     print(valid_result)
     logger.info(train_result)
     logger.info(valid_result)
-    file_path = "df1.txt"
-    with open(file_path, "w") as file:
-        file.write(train_result)
-        file.write(valid_result)
+    # file_path = "df1.txt"
+    # with open(file_path, "w") as file:
+    #     file.write(train_result)
+    #     file.write('/n')
+    #     file.write(valid_result)
     # wandb.log({"Standard BKT:": result})
     # wandb.finish()
     
-    # model2 = Model(seed = 0, num_fits = 20)
-    # model2.fit(data_path = "data/builder_train_preprocessed.csv", forgets=True)
-    # print("BKT+Forgets:", model2.evaluate(data_path = "data/builder_test_preprocessed.csv", metric="auc"))
+    model2 = Model(seed = 0, num_fits = 10)
+    model2.fit(data_path = "data/builder_train_preprocessed.csv", forgets=True)
+    print("BKT+Forgets:", model2.evaluate(data_path = "data/builder_test_preprocessed.csv", metric="auc"))
+    train_result_plus="BKT++ (train_auc):"+str(model2.evaluate(data_path = "data/df1_bkt_train.csv", metric="auc"))
+    valid_result_plus="BKT++ (valid_auc):"+str(model2.evaluate(data_path = "data/df1_bkt_test.csv", metric="auc"))
+    print(train_result_plus)
+    print(valid_result_plus)
+    logger.info(train_result_plus)
+    logger.info(valid_result_plus)
